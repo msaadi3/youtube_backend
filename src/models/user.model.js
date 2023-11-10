0eb980397b30cab2d583f699e8163ccd56608e1e
saad <saad@saad.comm>
2023-11-10 22:59:52 +0500

    user and video models

---
+++ src/models/user.model.js
@@ -0,0 +1,83 @@
+import { Schema, model } from "mongoose";
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+const userSchema = new Schema({
+    userName: {
+        type: String,
+        required: [true, 'user name is required'],
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true // for optimized search but don't overuse
+    },
+    email: {
+        type: String,
+        required: [true, 'email is required'],
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: [true, 'password is required'],
+    },
+    fullName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    avatar: {
+        type: String // cloudinary url
+    },
+    coverImage: {
+        type: String // cloudinary url
+    },
+    watchHistory: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Video'
+        }
+    ],
+    refreshToken: {
+        type: String
+    }
+}, { timeStamps: true })
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next()
+
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign({
+        _id: this._id,
+        userName: this.userName,
+        email: this.email,
+        fullName: this.fullName
+    },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign({
+        _id: this._id
+    },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = model('User', userSchema)
