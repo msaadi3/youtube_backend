8b5bc85bac3d245bfa9e9c4c398d54ef6c89ffe4
saad <saad@saad.comm>
2023-11-11 12:14:30 +0500

    utils and app

--- src/index.js
+++ src/index.js
@@ -1,40 +1,54 @@
 // second approach is to to create another file in db folder create function that would connect with db 
 // import connectDB from "./db/connectDB.js";
 import dotenv from 'dotenv'
+import app from './app'
 
 dotenv.config({
     path: './.env'
 })
 
-// connectDB()
+connectDB()
+    .then(() => {
+        app.on('error', (error) => {
+            console.log('error', error);
+            throw error
+        })
+
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`server is running on PORT: ${process.env.PORT}`);
+        })
+    })
+    .catch((error) => {
+        console.log('error: ', error);
+    })
 
 
 // First approach to connect with DB is to code an iifi in index.js file
-import mongoose from "mongoose";
-import { DBName } from './constants.js'
-import express from 'express'
-const app = express();
+// import mongoose from "mongoose";
+// import { DBName } from './constants.js'
+// import express from 'express'
+// const app = express();
 
-(async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DBName}`)
-            .then(() => {
-                console.log('connectd ');
-            })
-            .catch((err) => {
-                console.error('error ', err);
-                throw err
-            })
+// (async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DBName}`)
+//             .then(() => {
+//                 console.log('connectd ');
+//             })
+//             .catch((err) => {
+//                 console.error('error ', err);
+//                 throw err
+//             })
 
-        app.listen(process.env.PORT, () => {
-            console.log('app is listening on PORT: ', process.env.PORT);
-        })
+//         app.listen(process.env.PORT || 8000, () => {
+//             console.log('app is listening on PORT: ', process.env.PORT);
+//         })
 
-    } catch (error) {
-        console.error('error while connecting with database in index.js file ', error)
-        // process.exit(1)
-        throw error
-    }
+//     } catch (error) {
+//         console.error('error while connecting with database in index.js file ', error)
+//         // process.exit(1)
+//         throw error
+//     }
 
-})()
+// })()
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,23 @@
+// asyncHandler(A wrapper function) with Promises approach
+export const asyncHandler = (requestHandler) => {
+    (err, req, res, next) => {
+        Promise.resolve(requestHandler(err, req, res, next)).catch((error) => next(error))
+    }
+}
+
+
+
+// asyncHandler(A wrapper function) with try catch approach
+
+// const asyncHandler = (func) => { async () => { } }
+// const asyncHandler = (func) =>  async () => { } 
+// export const asyncHandler = (requestHandler) => async (err, req, res, next) => {
+//     try {
+//         await requestHandler(err, req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
